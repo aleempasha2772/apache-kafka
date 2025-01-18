f6af7d7a3414804d6d7a59f336c620c9764819b2
aleempasha2772 <aleempasha1410>
2025-01-18 11:03:10 +0530

    cab user and captain applicaiton

---
+++ cab-captain/src/main/java/com/example/cab_captain/CabCaptainApplication.java
@@ -0,0 +1,13 @@
+package com.example.cab_captain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabCaptainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabCaptainApplication.class, args);
+	}
+
+}
---
+++ cab-captain/src/test/java/com/example/cab_captain/CabCaptainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cab_captain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabCaptainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
